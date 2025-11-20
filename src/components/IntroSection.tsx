37d32ceed92f8ae8d0825c8139c22bc6776b4fa6
Sadat Ali <sa9621952@gmail.com>
2025-11-21 04:29:13 +0530

    Update cv link

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -96,7 +96,7 @@ const IntroSection = () => {
     };
   }, []);
   const downloadResume = () => {
-    const resumeUrl = "/my_cv.pdf"; // resume URL
+    const resumeUrl = "/my_cv_google.pdf"; // resume URL
     const link = document.createElement('a');
     link.href = resumeUrl;
     link.download = 'Sadat_Ali_Resume.pdf';
